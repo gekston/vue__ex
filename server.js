d2002f6e44146666691b896d4ef5df2a57a35b11
Nebeschenko Igor <nebeschenkoichor@gmail.com>
2017-10-08 21:32:38 +0300

    fix

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
+app.get('/style.js', function (req, res) {
+  res.sendFile(path.join(__dirname+'/style.js'));
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on http://localhost:3000/');
 });
